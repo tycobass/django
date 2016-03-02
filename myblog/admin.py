128cbb888344a3e4c036164cca3cea060e85c83e
Eric Vegors <tycobass@gmail.com>
2016-03-01 18:03:51 -0800

    before class

--- myblog/admin.py
+++ myblog/admin.py
@@ -10,22 +10,14 @@ admin.site.register(Category)
 # admin.site.register(Post) #<== commented to support inline esv
 
 class CategoryInline(admin.TabularInline):  #<== added esv
-    model = Category.posts.through
-    #model = models.ManyToManyField(Category)
-    # categories = models.ManyToManyField(Category)  #<== added esv
-    # model = categories  #<== added esv
+    model = Category.posts.through  #<== posts.through added esv
+
 
 class PostAdmin(admin.ModelAdmin):  #<== added esv
     inlines = [
         CategoryInline,
     ]
+    exclude = ('posts',)
 
-# class PostInline(admin.TabularInline):  #<== added esv
-#     model = Post
-
-# class CategoryInline(admin.ModelAdmin):  #<== added esv
-#     inlines = [
-#         PostInline
-#     ]
 
 admin.site.register(Post, PostAdmin) #<== commented to support inline esv
